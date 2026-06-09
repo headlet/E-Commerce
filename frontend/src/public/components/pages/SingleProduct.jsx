b9b392d9af250f9f3ccf790d3382c46febefb20d
headlet <professionalskill6@gmail.com>
2026-06-09 17:59:11 +0545

    buidling singleproduct page

---
+++ frontend/src/public/components/pages/SingleProduct.jsx
@@ -0,0 +1,14 @@
+function SingleProduct() {
+  return (
+    <section className="flex flex-col items-center justify-center w-full p-4 gap-5">
+      <div className="text-sm text-gray-500 w-full bg-white rounded-lg p-5">
+        Home / pages /{" "}
+        <span className="text-md font-semibold text-black">
+          Top Cell Phone & Tablet
+        </span>
+      </div>
+    </section>
+  );
+}
+
+export default SingleProduct;
--- frontend/src/public/components/routes/AppRoutes.jsx
+++ frontend/src/public/components/routes/AppRoutes.jsx
@@ -4,21 +4,23 @@ import Product from "../pages/Product";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Login from "../pages/Auth/logIn";
-import Signup from '../pages/Auth/signUp';
-import Profile from '../pages/Profile';
-import Cart from '../pages/Cart';
+import Signup from "../pages/Auth/signUp";
+import Profile from "../pages/Profile";
+import Cart from "../pages/Cart";
+import SingleProduct from "../pages/SingleProduct";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<Home />} />
       <Route path="/product" element={<Product />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path='/profile' element={<Profile/>}></Route>
-      <Route path='/cart' element={<Cart />}></Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/profile" element={<Profile />}></Route>
+      <Route path="/cart" element={<Cart />}></Route>
+      <Route path="/product/id" element={<SingleProduct/>}> </Route>
     </Routes>
   );
 }
