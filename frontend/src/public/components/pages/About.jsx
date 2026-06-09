a543b305212a0acca8429fdb18226b063f644177
headlet <professionalskill6@gmail.com>
2026-06-09 07:20:00 +0545

    complete about second section and building third section

--- frontend/src/public/components/pages/About.jsx
+++ frontend/src/public/components/pages/About.jsx
@@ -1,4 +1,5 @@
 import aboutimg from "../../../assets/div.about.png";
+import ban2 from '../../../assets/banner/aboutimg.png'
 function About() {
   return (
     <section className="flex flex-col items-center justify-center w-full p-4 gap-5">
@@ -9,65 +10,72 @@ function About() {
 
       {/* top hero section */}
       <div className="bg-white rounded-lg w-full">
-        <div>
-          <div className="w-full flex flex-col justify-center items-center lg:flex-row  md:justify-between border-b border-gray-400">
-            <div className="flex flex-col justify-center p-15 gap-5">
-              <h2 className="text-3xl font-bold">
-                Best experience{" "}
-                <span className="block text-xl font-normal ">always wins</span>
-              </h2>
-              <p className="text-sm text-gray-400 w-full ">
-                1st Online Marketplace for Shopping in Nepal
-              </p>
-            </div>
-            <img src={aboutimg} alt="" className="h-full w-[80%]" />
-          </div>
-
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-6 place-items-center gap-6">
-            <h2 className="text-lg font-semibold">
-              Our purpose is to enrich{" "}
-              <span className="text-[#1ABA1A] block"> and enhance lives</span>{" "}
-              through technology
+        <div className="w-full flex flex-col lg:flex-row items-center justify-between border-b border-gray-400">
+          <div className="flex flex-col justify-center items-center gap-5 lg:w-[28%] py-4">
+            <h2 className="text-3xl font-bold text-center">
+              Best experience
+              <span className="block text-xl font-normal">always wins</span>
             </h2>
 
-            <div>
-              <h2 className="text-2xl font-bold">$12,5 M</h2>
-              <p className="w-48 text-sm text-gray-500">
-                TOTAL REVENUE FROM 2020 -2026
-              </p>
-            </div>
-            <div>
-              <h2 className="text-2xl font-bold">12K+</h2>
-              <p className="w-48 text-sm text-gray-500">
-                ORDER DELIVERD SUCCESSFULON EVERYDAY
-              </p>
-            </div>
-            <div>
-              <h2 className="text-2xl font-bold">725+</h2>
-              <p className="w-48 text-sm text-gray-500">
-                STORE AND OFFICE IN NP AND WORLDWIDE
-              </p>
-            </div>
+            <p className="text-sm text-gray-400 px-5 text-center">
+              1st Online Marketplace for Shopping in Nepal
+            </p>
+          </div>
+
+          <img src={aboutimg} alt="" className="w-full lg:w-[60%] h-auto" />
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-6 place-items-center gap-6">
+          <h2 className="text-lg font-semibold">
+            Our purpose is to enrich{" "}
+            <span className="text-[#1ABA1A] block"> and enhance lives</span>{" "}
+            through technology
+          </h2>
+
+          <div>
+            <h2 className="text-2xl font-bold">$12,5 M</h2>
+            <p className="w-48 text-sm text-gray-500">
+              TOTAL REVENUE FROM 2020 -2026
+            </p>
+          </div>
+          <div>
+            <h2 className="text-2xl font-bold">12K+</h2>
+            <p className="w-48 text-sm text-gray-500">
+              ORDER DELIVERD SUCCESSFULON EVERYDAY
+            </p>
+          </div>
+          <div>
+            <h2 className="text-2xl font-bold">725+</h2>
+            <p className="w-48 text-sm text-gray-500">
+              STORE AND OFFICE IN NP AND WORLDWIDE
+            </p>
           </div>
         </div>
       </div>
 
       {/* second section */}
-      <div>
-        <img src="" alt="" />
-        <div>
-          <h2>
+      <div className="bg-white rounded-lg w-full p-6 grid grid-cols-1 md:grid-cols-2 place-items-center gap-2">
+        <img src={ban2} alt="" className=" h-[420px] rounded-lg"/>
+        <div className=" md:h-[420px] flex flex-col justify-center  gap-4 bg-gray-300 rounded-lg p-16">
+          <h2  className="text-lg font-bold ">
             We connect millions of buyers and sellers around the world,
             empowering people & creating economic opportunity for all.
           </h2>
-          <p>
+          <p className="text-sm">
             Within our markets, millions of people around the world connect,
             both online and offline, to make, sell and buy unique goods. We also
             offer a wide range of Seller Services and tools that help creative
             entrepreneurs start, manage & scale their businesses.
           </p>
-          <a href="" className="">OUR SHOWREEL</a>
+          <a href="" className="bg-[#1ABA1A] text-white p-2 rounded-lg w-fit">
+            OUR SHOWREEL
+          </a>
         </div>
+      </div>
+
+      {/* third section */}
+      <div>
+        
       </div>
     </section>
   );
