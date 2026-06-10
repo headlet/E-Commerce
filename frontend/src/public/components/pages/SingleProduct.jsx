10bb52101b76c12d67449f8eec6ea25be171954b
headlet <professionalskill6@gmail.com>
2026-06-10 07:27:51 +0545

    building singleproduct page

--- frontend/src/public/components/pages/SingleProduct.jsx
+++ frontend/src/public/components/pages/SingleProduct.jsx
@@ -8,8 +8,34 @@ function SingleProduct() {
         </span>
       </div>
 
-      <div className="w-full">
+      <div className="w-full bg-white rounded-lg">
+        <div>
+          <img src="" alt="" />
+          <div>
+            <img src="" alt="" />
+          </div>
+        </div>
 
+        <div>
+          <div>
+            <h2>Somseng Galatero X6 Ultra LTE 4G/128GB, Black Smartphone</h2>
+            <p>$300.00-$600.00</p>
+            <ul className="p-4 list-disc">
+              <li>
+                Intel LGA 1700 Socket: Supports 13th & 12th Gen Intel Core DDR5
+              </li>
+
+              <li>
+                Compatible: 4*SMD DIMMs with XMP 3.0 Memory Commanding Power
+              </li>
+              <li>Design: Twin 16+1+2 Phases Digital VRM</li>
+            </ul>
+
+            <p className="bg-green-100 rounded text-[#1ABA1A] w-fit">FREE SHIPPING</p>
+          </div>
+
+          <div></div>
+        </div>
       </div>
     </section>
   );
