e4072dd9a4d9a2896aabc75bdf70e843d454ad16
headlet <professionalskill6@gmail.com>
2026-07-21 18:39:34 +0545

    adding productservices with product image and its varients

--- backend/app/Http/Controllers/Services/ProductServices.php
+++ backend/app/Http/Controllers/Services/ProductServices.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Services;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Override;
 
 class ProductServices extends Services
 {
-    //
+
+    public function __construct(Product $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function getAllData(int $pagination)
+    {
+        return $this->model::with('image')->latest()->paginate($pagination);
+    }
+
+    public function getById(string $id)
+    {
+        $product = $this->model::with('image')->findOrFail($id);
+        return $product;
+    }
 }
